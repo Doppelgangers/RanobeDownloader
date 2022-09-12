f7ac020bb979898220fdecf7937c99ee6a7ce051
Doppelgangers <6666genocide666@mail.ru>
2022-09-12 19:59:01 +0300

    Update ConfigManager

--- settings.py
+++ settings.py
@@ -1,18 +1,48 @@
 import json
+import os
+
 
 class ConfigManager:
+    configs = {}
+    name_config_file = ''
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ConfigManager, cls).__new__(cls)
+        return cls.instance
 
-    @classmethod
-    def get_configs(cls):
-        with open('config.json', "r") as file:
-            return json.load(file)
+    def __init__(self, name_config_file:str = "config.json"):
+        self.name_config_file = name_config_file
+        self.configs = self.get_configs()
 
-    @classmethod
-    def edit_config(cls , name: str , value):
-        data = ConfigManager.get_configs()
-        data[name] = value
-        with open('config.json', "w") as file:
-            json.dump(data, file)
+        if self.configs.get("MP3SPLT_PATH") is None:
+            self.edit_config("MP3SPLT_PATH", 'mp3splt')
 
+        if self.configs.get("SAVE_TO") is None:
+            self.edit_config("SAVE_TO", os.path.dirname(os.path.abspath(__file__)))
+
+    def __str__(self):
+        self.configs = self.get_configs()
+        return self.configs
+
+    def get_configs(self):
+        try:
+            with open(self.name_config_file, "r") as file:
+                self.configs = json.load(file)
+                return self.configs
+        except FileNotFoundError:
+            with open(self.name_config_file, "w") as file:
+                json.dump({}, file)
+                return {}
+
+    def edit_config(self, name: str, value: str):
+
+        self.configs[name] = value
+        with open(self.name_config_file, "w") as file:
+            json.dump(self.configs, file)
+        return self.get_configs()
+
+
+if __name__ == "__main__":
+    pass
 
 
